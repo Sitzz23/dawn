5e6a60c4a4346f623804b6647cc61e284d0fe3a9
Sitanshu Pokalwar <sitz2309@gmal.com>
2024-06-18 20:32:23 +0530

    add: body content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
         <div className="container flex max-w-[64rem] flex-col items-center gap-5 text-center">
-          <h1 className=" text-4xl font-urban font-extrabold tracking-tight  sm:text-5xl md:text-6xl lg:text-7xl">
+          <h1 className=" text-4xl font-heading font-extrabold tracking-tight  sm:text-5xl md:text-6xl lg:text-7xl">
             <Balancer>
               Coding contests are competitive now, its{" "}
               <span className="relative bg-gradient-to-r from-indigo-500 to-purple-500/80 bg-clip-text font-extrabold text-transparent">
@@ -14,13 +14,11 @@ export default function Home() {
             </Balancer>
           </h1>
 
-          <p
-            className="max-w-[42rem] animate-fade-up leading-normal text-muted-foreground opacity-0 sm:text-xl sm:leading-8"
-            style={{ animationDelay: "0.35s", animationFillMode: "forwards" }}
-          >
+          <p className="max-w-[45rem] leading-normal text-muted-foreground  sm:text-xl sm:leading-8 font-figtree">
             <Balancer>
-              Empower your financial management with AI-driven insights, making
-              tracking and optimizing your finances effortless.
+              Enter the exhilarating world where coding meets competition in
+              virtual arenas that host real-time challenges against fellow
+              coders from around the globe
             </Balancer>
           </p>
         </div>
