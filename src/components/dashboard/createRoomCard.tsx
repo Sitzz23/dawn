41b1736b6dffc50cbc289185315dc03127199fb5
Sitanshu Pokalwar <sitz2309@gmail.com>
2024-06-25 20:15:14 +0530

    refactor code with reusable cards

--- src/components/dashboard/createRoomCard.tsx
+++ src/components/dashboard/createRoomCard.tsx
@@ -1,7 +1,90 @@
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 const CreateRoomCard = () => {
-  return <div>CreateRoomCard</div>;
+  return (
+    <Card className="w-[450px]">
+      <CardHeader>
+        <CardTitle>Create a battle</CardTitle>
+        <CardDescription>Set the stage for an epic battle!</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid grid-cols-2 gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="battleName">Battle Name</Label>
+              <Input id="battleName" placeholder="Enter battle name" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="maxPlayers">Max Players</Label>
+              <Select defaultValue={"2"}>
+                <SelectTrigger id="maxPlayers">
+                  <SelectValue placeholder="Select players" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="2">2 Players</SelectItem>
+                  <SelectItem value="4">4 Players</SelectItem>
+                  <SelectItem value="6">6 Players</SelectItem>
+                  <SelectItem value="8">8 Players</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="difficulty">Difficulty</Label>
+              <Select defaultValue="easy">
+                <SelectTrigger id="difficulty">
+                  <SelectValue placeholder="Select difficulty" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="easy">Easy</SelectItem>
+                  <SelectItem value="medium">Medium</SelectItem>
+                  <SelectItem value="hard">Hard</SelectItem>
+                  <SelectItem value="expert">Expert</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="roomDuration">Room Duration</Label>
+              <Select defaultValue="15">
+                <SelectTrigger id="roomDuration">
+                  <SelectValue placeholder="Select duration" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="15">15 minutes</SelectItem>
+                  <SelectItem value="30">30 minutes</SelectItem>
+                  <SelectItem value="45">45 minutes</SelectItem>
+                  <SelectItem value="60">1 hour</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline" className="font-urban font-bold">
+          Cancel
+        </Button>
+        <Button className="font-urban font-bold">Create Battle</Button>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default CreateRoomCard;
--- src/components/dashboard/joinRoomCard.tsx
+++ src/components/dashboard/joinRoomCard.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 const JoinRoomCard = () => {
-  return <div>JoinRoom</div>;
+  return (
+    <Card className="w-[350px] flex flex-col">
+      <CardHeader>
+        <CardTitle>Enter the battle</CardTitle>
+        <CardDescription>
+          Challenge awaits! Join an existing battle.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex-grow">
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Battle code</Label>
+              <Input id="name" placeholder="Enter the room Id" />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between ">
+        <Button variant="outline" className="font-urban font-bold">
+          Cancel
+        </Button>
+        <Button className="font-urban font-bold">Engage!</Button>
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default JoinRoomCard;
