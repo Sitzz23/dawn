3a248b16a1bacda02972a2d8b43b28cae660df12
Sitanshu Pokalwar <sitz2309@gmail.com>
2024-06-26 18:30:55 +0530

    fix: landing page clerk components

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,13 +1,8 @@
-// import {
-//   LoginLink,
-//   RegisterLink,
-// } from "@kinde-oss/kinde-auth-nextjs/components";
-// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import Balancer from "react-wrap-balancer";
+import { SignUpButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default async function Home() {
-  // const { getUser } = getKindeServerSession();
-  // const user = await getUser();
   return (
     <>
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
@@ -20,7 +15,6 @@ export default async function Home() {
               </span>
             </Balancer>
           </h1>
-
           <p className="max-w-[45rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8 font-figtree">
             <Balancer>
               Enter the exhilarating world where coding meets competition in
@@ -28,29 +22,29 @@ export default async function Home() {
               coders from around the globe
             </Balancer>
           </p>
-
           <div className="flex  justify-center space-x-2  md:space-x-4">
-            {/* {user ? (
+            <SignedIn>
               <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px font-semibold leading-6  text-white inline-block">
                 <span className="absolute inset-0 overflow-hidden rounded-full">
                   <span className="absolute inset-0 rounded-full bg-gradient-to-r from-indigo-500/0 via-violet-500/90 to-purple-500/0 opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
                 </span>
                 <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-2 px-4 ring-1 ring-white/10 ">
-                  <LoginLink>Dashboard</LoginLink>
+                  <Link href="/dashboard">Dashboard</Link>
                 </div>
                 <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-indigo-500/0 via-violet-400/90 to-purple-500/0 transition-opacity duration-500 group-hover:opacity-40"></span>
               </button>
-            ) : (
+            </SignedIn>
+            <SignedOut>
               <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px font-semibold leading-6  text-white inline-block">
                 <span className="absolute inset-0 overflow-hidden rounded-full">
                   <span className="absolute inset-0 rounded-full bg-gradient-to-r from-indigo-500/0 via-violet-500/90 to-purple-500/0 opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
                 </span>
                 <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-2 px-4 ring-1 ring-white/10 ">
-                  <RegisterLink>Get started</RegisterLink>
+                  <SignUpButton>Get started</SignUpButton>
                 </div>
                 <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-indigo-500/0 via-violet-400/90 to-purple-500/0 transition-opacity duration-500 group-hover:opacity-40"></span>
               </button>
-            )} */}
+            </SignedOut>
           </div>
         </div>
       </section>
