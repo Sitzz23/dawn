5cd163b2ad440bbc2c5a5095494906a32910deaf
Sitanshu Pokalwar <sitz2309@gmail.com>
2024-06-28 15:31:56 +0530

    add: socket handlers and events

--- src/pages/api/socket/io.ts
+++ src/pages/api/socket/io.ts
@@ -2,6 +2,10 @@ import { NextApiResponseServerIo } from "@/lib/types";
 import { NextApiRequest } from "next";
 import { Server as NetServer } from "http";
 import { Server as ServerIO } from "socket.io";
+import { ConvexHttpClient } from "convex/browser";
+import { api } from "../../../../convex/_generated/api";
+
+const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export const config = {
   api: {
@@ -17,18 +21,36 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
       path,
       addTrailingSlash: false,
     });
-    io.on("connection", (s) => {
-      s.on("create-room", (fileId) => {
-        s.join(fileId);
+
+    io.on("connection", (socket) => {
+      socket.on("join-room", async (roomId, userId) => {
+        socket.join(roomId);
+        await convex.mutation(api.room.addPlayerToRoom, { roomId, userId });
+        io.to(roomId).emit("player-joined", userId);
       });
-      s.on("send-changes", (deltas, fileId) => {
+
+      socket.on("leave-room", async (roomId, userId) => {
+        socket.leave(roomId);
+        await convex.mutation(api.room.removePlayerFromRoom, {
+          roomId,
+          userId,
+        });
+        io.to(roomId).emit("player-left", userId);
+      });
+
+      // Keep your existing socket event handlers
+      socket.on("create-room", (fileId) => {
+        socket.join(fileId);
+      });
+      socket.on("send-changes", (deltas, fileId) => {
         console.log("CHANGE");
-        s.to(fileId).emit("receive-changes", deltas, fileId);
+        socket.to(fileId).emit("receive-changes", deltas, fileId);
       });
-      s.on("send-cursor-move", (range, fileId, cursorId) => {
-        s.to(fileId).emit("receive-cursor-move", range, fileId, cursorId);
+      socket.on("send-cursor-move", (range, fileId, cursorId) => {
+        socket.to(fileId).emit("receive-cursor-move", range, fileId, cursorId);
       });
     });
+
     res.socket.server.io = io;
   }
   res.end();
