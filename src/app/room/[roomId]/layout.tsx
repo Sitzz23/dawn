e75347c9f7e3101bb2bbe0246bc7a7492f3d8144
Sitanshu Pokalwar <sitz2309@gmail.com>
2024-07-01 14:12:06 +0530

    add: room layout for providers

---
+++ src/app/room/[roomId]/layout.tsx
@@ -0,0 +1,15 @@
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { SocketProvider } from "@/lib/socketProvider";
+import React from "react";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <SocketProvider>
+        <TooltipProvider>{children}</TooltipProvider>
+      </SocketProvider>
+    </>
+  );
+};
+
+export default layout;
