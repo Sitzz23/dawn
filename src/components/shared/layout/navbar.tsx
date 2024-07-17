7b6c30f36081620af0f2eb9ddd146b15f3c4e853
Sitanshu Pokalwar <sitz2309@gmail.com>
2024-07-18 03:07:55 +0530

    fix: auth to db from navbar

--- src/components/shared/layout/navbar.tsx
+++ src/components/shared/layout/navbar.tsx
@@ -1,12 +1,27 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "../../ui/button";
-// import { UserNav } from "./userNav";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-// import { currentUser } from "@clerk/nextjs/server";
+import { useConvexAuth, useMutation } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import useUserStore from "@/store/userStore";
 
 const Navbar = () => {
-  // const user = await currentUser();
+  const { isAuthenticated } = useConvexAuth();
+  const addUser = useMutation(api.user.addUser);
+  const { setCurrentUserId } = useUserStore();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      return;
+    }
+    addUser({}).then((id) => {
+      // console.log(id);
+      setCurrentUserId(id);
+    });
+  }, [addUser, isAuthenticated, setCurrentUserId]);
+
   return (
     <div className="sticky top-0 z-30 flex h-16 items-center gap-10 border-b bg-background/60 px-4 backdrop-blur-xl transition-all">
       <Link href="/" className="flex items-center space-x-2">
@@ -23,17 +38,6 @@ const Navbar = () => {
             <SignInButton mode="modal">Sign In</SignInButton>
           </Button>
         </SignedOut>
-        {/* {user ? (
-          // <UserNav
-          //   email={user.emailAddresses[0].emailAddress as string}
-          //   name={user.fullName as string}
-          //   userImage={
-          //     user.imageUrl ?? `https://avatar.vercel.sh/${user.fullName}`
-          //   }
-          // />
-        ) : (
-         
-        )} */}
       </div>
     </div>
   );
