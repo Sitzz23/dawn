77da1d16b72630daf00358c13e6847c43b3a18c8
Sitanshu Pokalwar <sitz2309@gmail.com>
2024-07-19 03:32:55 +0530

    add: ui fixes

--- src/components/workspace/questionsSide/questions.tsx
+++ src/components/workspace/questionsSide/questions.tsx
@@ -4,7 +4,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import useQuestionStore from "@/store/questionsStore";
 import { formatString } from "@/lib/utils";
-import { Flag, Shuffle } from "lucide-react";
+import { Shuffle } from "lucide-react";
 import {
   Tooltip,
   TooltipContent,
@@ -19,95 +19,78 @@ const QuestionDisplay: React.FC = () => {
   if (!selectedQuestion) return <div>No question selected</div>;
 
   return (
-    <div className="">
-      <div className="mb-3 flex justify-between items-center">
-        <h2 className="text-2xl font-bold pr-2 text-pretty">
-          {selectedQuestion.title}
-        </h2>
-        <Tooltip>
-          <TooltipTrigger>
-            <Shuffle size={16} className="text-neutral-400" />
-          </TooltipTrigger>
-          <TooltipContent>Feature is under development</TooltipContent>
-        </Tooltip>
-      </div>
-
-      <div className="mb-12 flex justify-between items-center">
-        <div className="flex gap-2">
-          {selectedQuestion.tags.slice(0, 3).map((tag) => (
-            <Badge key={tag} variant="secondary" className="capitalize">
-              {formatString(tag)}
-            </Badge>
-          ))}
+    <div className="px-6 py-4 h-full flex flex-col">
+      <div className="sticky top-0 bg-background z-10 pb-4">
+        <div className="mb-3 flex justify-between items-center">
+          <h2 className="text-2xl font-bold pr-2 text-pretty">
+            {selectedQuestion.title}
+          </h2>
+          <Tooltip>
+            <TooltipTrigger>
+              <Shuffle size={16} className="text-neutral-400" />
+            </TooltipTrigger>
+            <TooltipContent>Feature is under development</TooltipContent>
+          </Tooltip>
         </div>
-        <Badge variant={selectedQuestion.difficulty} className="capitalize">
-          {selectedQuestion.difficulty}
-        </Badge>
-      </div>
 
-      <div className=" mb-6">
-        <h2 className="font-bold text-lg">Problem Statement</h2>
-        <p>{selectedQuestion.problemStatement}</p>
-      </div>
-
-      {selectedQuestion.constraints &&
-        selectedQuestion.constraints.length > 0 && (
-          <div className="mb-6">
-            <h2 className="font-bold text-lg">Constraints</h2>
-
-            <ul className="list-disc pl-5">
-              {selectedQuestion.constraints.map((constraint, index) => (
-                <li key={index}>{constraint}</li>
-              ))}
-            </ul>
-          </div>
-        )}
-
-      {selectedQuestion.examples.map((example, index) => (
-        <div className="mb-6 space-y-1" key={index}>
-          <h2 className="font-bold text-lg">Example {index + 1}</h2>
-          <div className="mb-2 grid grid-cols-3 bg-neutral-500/10 rounded-md p-3 px-4">
-            <div className="space-y-1">
-              <p className="font-semibold">Input:</p>
-              <p className="font-semibold">Output:</p>
-              <p className="font-semibold">Explanation:</p>
-            </div>
-            <div className="space-y-1 col-span-2 ">
-              <p>
-                <code className="text-sm">{example.input}</code>
-              </p>
-              <p>
-                <code className="text-sm">{example.output}</code>
-              </p>
-              <p>
-                <code className="text-sm">{example.explanation}</code>
-              </p>
-            </div>
-          </div>
-        </div>
-      ))}
-
-      {/* <div className="mb-6">
-        <Tabs defaultValue="1">
-          <TabsList>
-            {selectedQuestion.testCases.map((_, index) => (
-              <TabsTrigger key={index} value={`${index + 1}`}>
-                Test Case {index + 1}
-              </TabsTrigger>
+        <div className="flex justify-between items-center">
+          <div className="flex gap-2">
+            {selectedQuestion.tags.slice(0, 3).map((tag) => (
+              <Badge key={tag} variant="secondary" className="capitalize">
+                {formatString(tag)}
+              </Badge>
             ))}
-          </TabsList>
-          {selectedQuestion.testCases.map((testCase, index) => (
-            <TabsContent key={index} value={`${index + 1}`}>
-              <p>
-                <strong>Input:</strong> {testCase.input}
-              </p>
-              <p>
-                <strong>Output:</strong> {testCase.output}
-              </p>
-            </TabsContent>
+          </div>
+          <Badge variant={selectedQuestion.difficulty} className="capitalize">
+            {selectedQuestion.difficulty}
+          </Badge>
+        </div>
+      </div>
+
+      <div className="overflow-y-auto flex-1">
+        <div className="pt-4">
+          <div className="mb-6">
+            <h2 className="font-bold text-lg">Problem Statement</h2>
+            <p>{selectedQuestion.problemStatement}</p>
+          </div>
+
+          {selectedQuestion.constraints &&
+            selectedQuestion.constraints.length > 0 && (
+              <div className="mb-6">
+                <h2 className="font-bold text-lg">Constraints</h2>
+                <ul className="list-disc pl-5">
+                  {selectedQuestion.constraints.map((constraint, index) => (
+                    <li key={index}>{constraint}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
+
+          {selectedQuestion.examples.slice(0, 1).map((example, index) => (
+            <div className="mb-6 space-y-1" key={index}>
+              <h2 className="font-bold text-lg">Example {index + 1}</h2>
+              <div className="mb-2 grid grid-cols-6 bg-neutral-500/10 rounded-md p-2 px-3">
+                <div className="space-y-1 col-span-2">
+                  <p className="font-semibold">Input:</p>
+                  <p className="font-semibold">Output:</p>
+                  <p className="font-semibold">Explanation:</p>
+                </div>
+                <div className="space-y-1 col-span-4 ">
+                  <p>
+                    <code className="text-sm">{example.input}</code>
+                  </p>
+                  <p>
+                    <code className="text-sm">{example.output}</code>
+                  </p>
+                  <p>
+                    <code className="text-sm">{example.explanation}</code>
+                  </p>
+                </div>
+              </div>
+            </div>
           ))}
-        </Tabs>
-      </div> */}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/workspace/questionsSide/questionsSide.tsx
+++ src/components/workspace/questionsSide/questionsSide.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import QuestionDisplay from "./questions";
 
 const QuestionSide = () => {
   return (
-    <div className="flex h-full p-2 rounded-lg ">
+    <div className=" ">
       <QuestionDisplay />
     </div>
   );
