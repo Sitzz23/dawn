19ca4abafc3bb9497e74889a450b284a4cdbd7cf
Sitanshu Pokalwar <sitz2309@gmail.com>
2024-07-31 19:01:50 +0530

    add: empty state test case and tabs

--- src/components/empty/emptyTestCases.tsx
+++ src/components/empty/emptyTestCases.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const EmptyTestCases = () => {
-  return <div className="p-6">EmptyTestCases</div>;
+  return <div className="p-6">Loading test cases...</div>;
 };
 
 export default EmptyTestCases;
